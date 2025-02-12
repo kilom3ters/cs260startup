529d98142f81af48afd79fde6a2a1c3d5740de5c
kilom3ters <mwJohnson404@gmail.com>
2025-02-11 20:27:14 -0700

    fixed css for home/app

--- src/app.jsx
+++ src/app.jsx
@@ -8,7 +8,14 @@ import { Home } from './home/home';
 
 
 export default function App() {
-  return ( <BrowserRouter> ( <div>)
+  return ( <BrowserRouter>  <div><nav className="navbar bg-white">
+    <div className="container-fluid">
+      <form className="d-flex ms-auto" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search Users" aria-label="Search"></input>
+        <button className="btn btn-outline-dark" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
 
 <Routes>
  <Route path='/' element={<Home />} exact />
@@ -18,10 +25,12 @@ export default function App() {
 </Routes>
 
 <footer>
+  <div className="footer">
     <div className="container">
           <div className="container-fluid">
             <p>Miles Johnson: <a href = "https://github.com/kilom3ters/cs260startup/tree/main"> GitHub Repository</a></p>
           </div>
+    </div>
     </div>
     </footer>
 </div>
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -7,12 +7,12 @@ export function Home() {
 
   return (
     <main className="container-fluid bg-secondary text-center vh-100">
-      <div className="row h-100">
+      <div className="left-side">
         <div className="col-md-6 d-flex flex-column justify-content-center bg-black text-white p-5">
           <h1>Welcome to GameLog</h1>
           <p>This is the best place for connecting with friends, tracking your favorite games, and more.</p>
         </div>
-
+      <div className="right-side">
         <div className="col-md-6 d-flex flex-column justify-content-center align-items-center p-5">
           <button onClick={() => navigate('/login')} className="btn btn-dark btn-lg w-50 mb-3">
             Create an Account
@@ -21,6 +21,7 @@ export function Home() {
             Login
           </button>
         </div>
+      </div>
       </div>
     </main>
   );
